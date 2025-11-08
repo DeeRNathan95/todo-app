4b54b4f35e111dadd14aa55b734ff4f1c22e8b13
Dee <d.r.nathan@outlook.com>
2025-11-08 16:14:32 +0100

    added inputs for todos

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import { TodoInput } from "./components/TodoInput"
 import { TodoList } from "./components/TodoList"
 
 function App() {
-  let x = 3
+  const todos = [
+    {input: 'Hello! Add your first todo!', complete: true},
+    {input: 'Get the groceries!', complete: false},
+    {input: 'Learn how to web design!', complete: false},
+    {input: 'Say hi to gran gran!', complete: true},
+  ]
+
   return (
       <>
       <Header />
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -2,11 +2,13 @@ export function Tabs() {
 
     const tabs = ['All', 'Open','Completed']
     return(
-        <nav>
+        <nav className="tab-container">
             {tabs.map((tab, tabIndex) => { //first parameter is the current value of the array. Second parameter is the index of the array, Third which is optional is the whole array.
                 return (
-                    <button key={tabIndex}>
-                        <h4>{tab}</h4> 
+                    <button key={tabIndex}
+                    className="tab-button">
+                        <h4>{tab} <span>(0)
+                            </span></h4> 
                         </button>
                 )
             })}
